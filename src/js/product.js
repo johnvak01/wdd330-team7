edef336b11885b6358e771f54796d35b94f6c515
Dani-3108 <floresd477@gmail.com>
2026-07-05 22:43:07 -0600

    W01 Individual Activity: Add Item to Cart completed

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,19 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { getLocalStorage } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cart = getLocalStorage("so-cart");
+  let cartArray;
+  if (cart == null) {
+    cartArray = [product];
+  } else {
+    cart.push(product);
+    cartArray = cart;
+  }
+  setLocalStorage("so-cart", cartArray);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -16,3 +25,4 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+//setLocalStorage("so-cart", product);
