62c6bd27b6d7aa9ac163163dff3f03e9e8fc1233
johnvak01 <johnvak01@gmail.com>
2026-07-08 19:24:02 -0600

    fixed linting errors

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,7 @@
-import { setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProjectDetails.mjs";
+
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
@@ -9,9 +10,9 @@ const product = new ProductDetails(productId, dataSource);
 product.init();
 
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// async function addToCartHandler(e) {
+//   const productData = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(productData);
+// }
 
 // add listener to Add to Cart button
