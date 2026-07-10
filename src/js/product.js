903b9b6be741104116a01630b015f78ba7aab271
johnvak01 <johnvak01@gmail.com>
2026-07-09 19:10:06 -0600

    fixed issue with team project

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProjectDetails.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
@@ -12,6 +12,10 @@ product.init();
 // async function addToCartHandler(e) {
 //   const productData = await dataSource.findProductById(e.target.dataset.id);
 //   addProductToCart(productData);
+//     console.log("test");
 // }
 
-// add listener to Add to Cart button
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
