291d44f2e2e8f9b3b20b0319e5a23770a7a34689
johnvak01 <johnvak01@gmail.com>
2026-07-11 11:59:45 -0600

    inserted discount indicator to product page

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -25,7 +25,10 @@ export default class ProductDetails {
         productImage.src = this.product.Image;
         productImage.alt = this.product.NameWithoutBrand;
       
-        document.querySelector('.product-card__price').textContent = this.product.FinalPrice;
+        document.querySelector('.product-card__price').textContent = "$" + this.product.FinalPrice;
+        if (this.product.FinalPrice < this.product.SuggestedRetailPrice) {
+            document.querySelector('.product-card__price').innerHTML += `<span class="product-card__price--sale">: Discounted ${((this.product.FinalPrice/this.product.SuggestedRetailPrice)*100).toFixed(0)}%!</span>`;
+        }
         document.querySelector('.product__color').textContent = this.product.Colors[0].ColorName;
         document.querySelector('.product__description').innerHTML = this.product.DescriptionHtmlSimple;
       
