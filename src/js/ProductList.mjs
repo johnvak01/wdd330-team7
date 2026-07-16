f7cde4d77f20b37e5edfd3f36cfbc1d757262c2e
julian.cuervo <julian.cuervo@paxzu.co>
2026-07-16 12:41:13 -0400

    feat: W02 Individual Activity: Dynamic Product List

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,26 +1,4 @@
 import { renderListWithTemplate } from "./utils.mjs";
-
-function productCardTemplate(product) {
-  let discountText = "";
-  if (product.FinalPrice < product.SuggestedRetailPrice) {
-    discountText = "product with discount";
-  }
-  return `
-    <li class="product-card">
-      <a href="product_pages/?product=${product.Id}">
-        <img
-            src="${product.Image}"
-            alt="Image of ${product.NameWithoutBrand}"
-        />
-        <h3 class="card__brand">${product.Brand.Name}</h3>
-        <h2 class="card__name">${product.NameWithoutBrand}</h2>
-        <p class="product-card__price">$${product.FinalPrice}</p>
-        <p class="product-card_price_discount">${discountText}</p>
-      </a>
-    </li>
-  `;
-}
-
 export default class ProductList {
   constructor(category, dataSource, listElement) {
     this.category = category;
@@ -33,9 +11,33 @@ export default class ProductList {
     this.renderList(list);
   }
 
+  
+  _productCardTemplate(product) {
+  let discountText = "";
+  if (product.FinalPrice < product.SuggestedRetailPrice) {
+    discountText = "product with discount";
+  }
+  return `
+    <li class="product-card">
+      <a href="product_pages/?product=${product.Id}">
+        <img
+            src="${product.Image}"
+            onerror="this.src='../images/tents/no-image-available.webp'"
+            alt="Image of ${product.NameWithoutBrand}"
+        />
+        <h3 class="card__brand">${product.Brand.Name}</h3>
+        <h2 class="card__name">${product.NameWithoutBrand}</h2>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+        <p class="product-card_price_discount">${discountText}</p>
+      </a>
+    </li>
+  `;
+}
+
+
   renderList(productList) {
     renderListWithTemplate(
-      productCardTemplate,
+      this._productCardTemplate,
       this.listElement,
       productList,
       "afterbegin",
