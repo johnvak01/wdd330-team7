9c8f03b5d6e9fbc0f1e152a61d09f5e609a83bc6
Danae De la Cruz <danae_taiyaki@hotmail.com>
2026-07-17 15:43:13 -0600

    add change

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
@@ -8,6 +8,7 @@ const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
+loadHeaderFooter();
 // add to cart button event handler
 // async function addToCartHandler(e) {
 //   const productData = await dataSource.findProductById(e.target.dataset.id);
