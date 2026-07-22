6006d5dacfaf9d52f843b0505c40a6db1cc11442
johnvak01 <johnvak01@gmail.com>
2026-07-22 17:10:07 -0600

    updates ProductData to ExternalServices

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,4 +1,4 @@
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import ProductSort from "./ProductSort.mjs";
 import { loadHeaderFooter, getParam } from "./utils.mjs";
@@ -17,7 +17,7 @@ if (title && category) {
   title.textContent = `Top Products: ${formattedCategory}`;
 }
 
-const productData = new ProductData();
+const productData = new ExternalServices();
 
 const productList = new ProductList(
   category,
