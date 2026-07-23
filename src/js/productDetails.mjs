534ea5bd0f06f85eaa586bf3bc71f2341af766b0
Dani-3108 <floresd477@gmail.com>
2026-07-22 18:24:51 -0600

    changes

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,68 +1,37 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { setLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
-
-  constructor(productId, dataSource) {
-    this.productId = productId;
-    this.product = {};
-    this.dataSource = dataSource;
-  }
-
-  
-
-  async init() {
-    // use the datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-    this.product = await this.dataSource.findProductById(this.productId);
-    // the product details are needed before rendering the HTML
-    this.renderProductDetails();
-    // once the HTML is rendered, add a listener to the Add to Cart button
-    // Notice the .bind(this). This callback will not work if the bind(this) is missing. Review the readings from this week on "this" to understand why.
-    document
-      .getElementById("addToCart")
-      .addEventListener("click", this.addProductToCart.bind(this));
-  }
-
-  addProductToCart() {
-    const cartItems = getLocalStorage("so-cart") || [];
-    cartItems.push(this.product);
-    setLocalStorage("so-cart", cartItems);
-  }
-
-  renderProductDetails() {
-    productDetailsTemplate(this.product);
-  }
-}
-
-function productDetailsTemplate(product) {
-  document.querySelector("h2").textContent = product.Brand.Name;
-  document.querySelector("h3").textContent = product.NameWithoutBrand;
-
-  const productImage = document.getElementById("productImage");
-  productImage.src = product.Image;
-  productImage.alt = product.NameWithoutBrand;
-
-  document.getElementById("productPrice").textContent = product.FinalPrice;
-  document.getElementById("productColor").textContent = product.Colors[0].ColorName;
-  document.getElementById("productDesc").innerHTML = product.DescriptionHtmlSimple;
-
-  document.getElementById("addToCart").dataset.id = product.Id;
-}
-
-// ************* Alternative Display Product Details Method *******************
-// function productDetailsTemplate(product) {
-//   return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
-//     <h2 class="divider">${product.NameWithoutBrand}</h2>
-//     <img
-//       class="divider"
-//       src="${product.Image}"
-//       alt="${product.NameWithoutBrand}"
-//     />
-//     <p class="product-card__price">$${product.FinalPrice}</p>
-//     <p class="product__color">${product.Colors[0].ColorName}</p>
-//     <p class="product__description">
-//     ${product.DescriptionHtmlSimple}
-//     </p>
-//     <div class="product-detail__add">
-//       <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-//     </div></section>`;
-// }
+    constructor(productId, dataSource) {
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
+    async init() {
+        this.product = await this.dataSource.findProductById(this.productId);
+        this.renderProductDetails();
+        document.getElementById("addToCart")
+        .addEventListener("click", this.addProductToCart.bind(this));
+    }
+    addProductToCart() {
+        let tasks = JSON.parse(localStorage.getItem("so-cart")) || [];
+        tasks.push(this.product);
+        setLocalStorage("so-cart", tasks);
+    }
+    renderProductDetails() {
+        document.querySelector("h2").textContent = this.product.Brand.Name;
+        document.querySelector("h3").textContent = this.product.NameWithoutBrand;
+      
+        const productImage = document.getElementById("productImage");
+        productImage.src = this.product.Images.PrimaryLarge;
+        productImage.alt = this.product.NameWithoutBrand;
+      
+        document.querySelector(".product-card__price").textContent = "$" + this.product.FinalPrice;
+        if (this.product.FinalPrice < this.product.SuggestedRetailPrice) {
+            document.querySelector(".product-card__price").innerHTML += `<span class="product-card__price--sale">: Discounted ${((this.product.FinalPrice / this.product.SuggestedRetailPrice) * 100).toFixed(0)}%!</span>`;
+        }
+        document.querySelector(".product__color").textContent = this.product.Colors[0].ColorName;
+        document.querySelector(".product__description").innerHTML = this.product.DescriptionHtmlSimple;
+      
+        document.getElementById("addToCart").dataset.id = this.product.Id;
+    }
+};
