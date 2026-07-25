b360c9a13b66e0d61c68d5e434f864f5bff8e94c
johnvak01 <johnvak01@gmail.com>
2026-07-25 07:02:15 -0600

    added scripting and css for cart superscript to bounce and update automatically.

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,4 +1,4 @@
-import { setLocalStorage, alertMessage } from "./utils.mjs";
+import { setLocalStorage, alertMessage, loadHeaderFooter } from "./utils.mjs";
 
 
 export default class ProductDetails {
@@ -11,29 +11,42 @@ export default class ProductDetails {
         this.product = await this.dataSource.findProductById(this.productId);
         this.renderProductDetails();
         document.getElementById("addToCart")
-        .addEventListener("click", this.addProductToCart.bind(this));
+            .addEventListener("click", () => {
+                const cart_superscript = document.querySelector(".cart_superscript");
+                cart_superscript.classList.add("cart-pulse");
+                cart_superscript.addEventListener('animationend', () => {
+                    cart_superscript.classList.remove('cart-pulse');
+                }, { once: true });
+            });
+        document.getElementById("addToCart")
+            .addEventListener("click", this.addProductToCart.bind(this));
     }
     addProductToCart() {
         let tasks = JSON.parse(localStorage.getItem("so-cart")) || [];
         tasks.push(this.product);
         setLocalStorage("so-cart", tasks);
         alertMessage("Product added to cart!");
+        document.querySelector(".cart_superscript").textContent = tasks.length;
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          });
     }
     renderProductDetails() {
         document.querySelector("h2").textContent = this.product.Brand.Name;
         document.querySelector("h3").textContent = this.product.NameWithoutBrand;
-      
+
         const productImage = document.getElementById("productImage");
         productImage.src = this.product.Images.PrimaryLarge;
         productImage.alt = this.product.NameWithoutBrand;
-      
+
         document.querySelector(".product-card__price").textContent = "$" + this.product.FinalPrice;
         if (this.product.FinalPrice < this.product.SuggestedRetailPrice) {
-          document.querySelector(".product-card__price").innerHTML += `<span class="product-card__price--sale">: 🏷️ Discounted ${((this.product.FinalPrice / this.product.SuggestedRetailPrice) * 100).toFixed(0)}%!</span>`;
+            document.querySelector(".product-card__price").innerHTML += `<span class="product-card__price--sale">: 🏷️ Discounted ${((this.product.FinalPrice / this.product.SuggestedRetailPrice) * 100).toFixed(0)}%!</span>`;
         }
         document.querySelector(".product__color").textContent = this.product.Colors[0].ColorName;
         document.querySelector(".product__description").innerHTML = this.product.DescriptionHtmlSimple;
-      
+
         document.getElementById("addToCart").dataset.id = this.product.Id;
     }
 };
